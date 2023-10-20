39a547558f6b9f097f2649905f785c7ee13c9c70
Mikaela-D <G00401392@atu.ie>
2023-10-20 19:58:24 +0100

    Added code to RegistrationController

--- src/main/java/ie/atu/lab3_registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/lab3_registrationservice/RegistrationController.java
@@ -1,4 +1,26 @@
 package ie.atu.lab3_registrationservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RegistrationController {
+    private final NotificationServiceClient notificationServiceClient;
+    @Autowired
+    public RegisterController(NotificationServiceClient notificationServiceClient) {
+        this.notificationServiceClient = notificationServiceClient;
+    }
+
+    @PostMapping("/confirm")
+    public String confirmDetails(@RequestBody UserDetails userDetails){
+        return String.format("Received details for %s with email %s", userDetails.getName(), userDetails.getEmail());
+    }
+    @PostMapping("/notification-received")
+    public String notificationReceived(@RequestBody UserDetails userDetails){
+        String notificationConfirm = notificationServiceClient.notificationConfirmation(userDetails);
+        String message = notificationConfirm + "\nThank you" + userDetails.getName() + "Here is the notification on this email: "+ userDetails.getEmail();
+        return message;
+    }
 }
