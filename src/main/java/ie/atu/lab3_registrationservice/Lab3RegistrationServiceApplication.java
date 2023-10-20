2b9114a6be4a337a706aee842de265484bc615c9
Mikaela-D <G00401392@atu.ie>
2023-10-20 19:41:56 +0100

    Created new project RegistrationService

---
+++ src/main/java/ie/atu/lab3_registrationservice/Lab3RegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3_registrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3RegistrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3RegistrationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab3_registrationservice/Lab3RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3_registrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3RegistrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
