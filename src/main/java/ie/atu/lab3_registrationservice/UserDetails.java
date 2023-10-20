b1081f2de41789328e58c4e1a97e287ccf13d45e
Mikaela-D <G00401392@atu.ie>
2023-10-20 19:44:09 +0100

    Added attributes to UserDetails

--- src/main/java/ie/atu/lab3_registrationservice/UserDetails.java
+++ src/main/java/ie/atu/lab3_registrationservice/UserDetails.java
@@ -1,4 +1,14 @@
 package ie.atu.lab3_registrationservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class UserDetails {
+    private String name;
+    private String email;
 }
