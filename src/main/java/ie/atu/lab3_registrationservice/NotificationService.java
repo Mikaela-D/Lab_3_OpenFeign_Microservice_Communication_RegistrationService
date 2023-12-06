6acce1dcd030fa9b5eb899068e7a10e34ea973ff
Mikaela <G00401392@atu.ie>
2023-12-06 14:35:55 +0000

    Added Dockerfile

--- src/main/java/ie/atu/lab3_registrationservice/NotificationService.java
+++ src/main/java/ie/atu/lab3_registrationservice/NotificationService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "received", url = "http://localhost:8083")
+@FeignClient(name = "received", url = "http://localhost:8081")
 public interface NotificationService {
     @PostMapping("/received")
     String notificationConfirmation(@RequestBody UserDetails userDetails);
